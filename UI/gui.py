59e1e1024448aeddedb7615dba6af535571f67d0
Luiz Ibrahim <luizibrahim94@gmail.com>
2023-06-01 23:02:38 -0300

    UI-FirstSteps

--- UI/gui.py
+++ UI/gui.py
@@ -1,12 +1,17 @@
 import PySimpleGUI as sg      
 
-sg.theme('DarkAmber')    # Keep things interesting for your users
+sg.theme('Black')    # General Theme
 
-layout = [[sg.Text('Persistent window')],      
-          [sg.Input(key='-IN-')],      
-          [sg.Button('Read'), sg.Exit()]]      
+right_menu_layout = [[sg.Button('Acessar Camera')],
+          [sg.Button('Acessar Logs')],
+          [sg.Button('Lista de Usuários')],
+          [sg.Button('Cadastrar Novo Usuário')],
+          ]      
 
-window = sg.Window('Window that stays open', layout)      
+layout = [
+    [sg.Column(right_menu_layout, element_justification='right', expand_x = True)]
+]
+window = sg.Window('Main Page', layout, size=(1280,720))      
 
 while True:                             # The Event Loop
     event, values = window.read() 
