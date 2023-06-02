f0b9b144dd5d1b8ebb706d3c419452b1d70d1406
Luiz Ibrahim <luizibrahim94@gmail.com>
2023-06-02 00:22:57 -0300

    EntradaDeLogs

--- projeng.py
+++ projeng.py
@@ -3,6 +3,8 @@ import face_recognition as fr
 import numpy as np
 from datetime import datetime
 import os
+from time import sleep
+
 
 camera = cv2.VideoCapture(0)
 rodando = True
@@ -45,9 +47,6 @@ def cortaRosto(faces_detect, frame):
         loc_h = h
     return rosto_img, loc_x, loc_y, loc_w, loc_h
     
-    
-    
-    
 for diretorio, subpastas, arquivos in os.walk(pasta):
     for arquivo in arquivos:
         nome = os.path.splitext(arquivo)[0]
@@ -61,50 +60,86 @@ for diretorio, subpastas, arquivos in os.walk(pasta):
 
 qtd_pessoas = np.shape(nomes)[0]
 
+registros_entrada = []
+registros_saida = []
+nomes_entrada = []
+
 #rodando = False
 while rodando:
     aux_compare = 0
     status, frame = camera.read()
     frame = cv2.resize(frame, (640,480), interpolation = cv2.INTER_AREA)
     
-    faces_detect, detection = procuraRosto(frame)
-    if detection == "found":
-        rosto, loc_x, loc_y, loc_w, loc_h = cortaRosto(faces_detect, frame)
-        rosto_test = cv2.cvtColor(rosto,cv2.COLOR_BGR2RGB)
-        
-        if np.shape(fr.face_encodings(rosto_test))[0]==0:
-            print('wait...')
-            pass
-        else:
-            encodeTest = fr.face_encodings(rosto_test)[0]
-            
-            while aux_compare < (qtd_pessoas):
-                encodeTrain = faces[aux_compare]
-                comparacao = fr.compare_faces([encodeTrain],encodeTest,tolerance=0.48) #Comparação Face Atual X Face Treinada.
-                distancia = fr.face_distance([encodeTrain],encodeTest)
-                print(comparacao,distancia)
-            
-                if comparacao == [True]:
-                    nome = nomes[aux_compare]
-                    try: 
-                        cv2.rectangle(frame, (loc_x, loc_y), (loc_x+loc_w, loc_y+loc_h) ,(0,255,0),2)
-                        org = (loc_x, loc_y)
-                        cv2.putText(frame, nome, org , font, fontScale, color, thickness, cv2.LINE_AA, False)
-                        break
-                    except:
-                        print('erro ao inserir credenciais...')
-                        pass
-                else:
-                    aux_compare = aux_compare + 1
+    aux_detect = 0
+    aux_sleep_box = 0
+    if cv2.waitKey(1) & 0xff == ord('d'):
+        while aux_detect < 5:
+            faces_detect, detection = procuraRosto(frame)
+            if detection == "found":
+                rosto, loc_x, loc_y, loc_w, loc_h = cortaRosto(faces_detect, frame)
+                rosto_test = cv2.cvtColor(rosto,cv2.COLOR_BGR2RGB)
                 
+                if np.shape(fr.face_encodings(rosto_test))[0]==0:
+                    print('USUÁRIO DESCONHECIDO - ACESSO NEGADO')
+                    pass
+                else:
+                    encodeTest = fr.face_encodings(rosto_test)[0]
+                    
+                    while aux_compare < (qtd_pessoas):
+                        encodeTrain = faces[aux_compare]
+                        comparacao = fr.compare_faces([encodeTrain],encodeTest,tolerance=0.48) #Comparação Face Atual X Face Treinada.
+                        distancia = fr.face_distance([encodeTrain],encodeTest)
+                        #print(comparacao,distancia)
+                    
+                        if comparacao == [True]:
+                            nome = nomes[aux_compare]
+                            try: 
+                                detect = frame
+                                cv2.rectangle(detect, (loc_x, loc_y), (loc_x+loc_w, loc_y+loc_h) ,(0,255,0),2)
+                                org = (loc_x, loc_y)
+                                cv2.putText(detect, nome, org , font, fontScale, color, thickness, cv2.LINE_AA, False)
+
+
+                                
+                                if nome in nomes_entrada:
+                                    registros_saida.append([nome, agora])
+                                    index_nome = nomes_entrada.index(nome)
+                                    agora = datetime.now()
+                                    tempo_permanencia = agora - (registros_entrada[index_nome][1])
+                                    print("SAIDA REGISTRADA: "+nome+" - TEMPO DE PERMANENCIA: "+str(tempo_permanencia))
+
+                                    pass
+                                else:
+                                    agora = datetime.now()
+                                    nomes_entrada.append(nome)
+                                    registros_entrada.append([nome, agora])
+                                
+
+                                print("ACESSO LIBERADO: "+str(nome))
+                                # print(registros_entrada)
+                                # print(nomes_entrada)
+                                sleep(2)
+                                
+                                break
+                            except:
+                                print('erro ao inserir credenciais...')
+                                comparacao = False
+                                pass
+                        else:
+                            aux_compare = aux_compare + 1
+
+                
+                break
+
+            aux_detect = aux_detect + 1 
+            if aux_detect == 5 :
+                print("NÃO FOI POSSIVEL IDENTIFICAR")          
     if not status or cv2.waitKey(1) & 0xff == ord('q'):
         rodando = False
         
     cv2.imshow('Janela_cam', frame)
+    #print("aux_sleep_box = "+str(aux_sleep_box))
+
 
 camera.release()
 cv2.destroyAllWindows()
-
-
-
-#Comentei essa outra linhas
